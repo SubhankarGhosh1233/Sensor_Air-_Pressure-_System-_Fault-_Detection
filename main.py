ba465f5b24ade962060776448e7d1f829b2768d0
Subhankar Ghosh <subhankar333ghosh@gmail.com>
2025-06-30 14:50:54 +0530

    Third Commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import pymongo
+# 
+client = pymongo.MongoClient("mongodb+srv://subhankar:subhankar@cluster1.xsd7qsq.mongodb.net/")
+## Define Database and Collection
+#dataBase = client[""]
+#
+#collection=dataBase[""]
+
