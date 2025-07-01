8c590b6b61c7085a0303ef31584e76a6a6128b2e
Subhankar Ghosh <subhankar333ghosh@gmail.com>
2025-07-01 19:37:46 +0530

    modified:main.py, modified:sensor/components/data_ingestion.py ,modified:sensor/entity/artifact_entity.py ,modified:sensor/entity/config_entity.py

--- main.py
+++ main.py
@@ -2,22 +2,33 @@ from sensor.logger import logging
 from sensor.exception import SensorException
 from sensor.utils import get_collection_as_dataframe
 import sys,os
+from sensor.entity import config_entity
+#from sensor.components.data_ingestion import DataIngestion
 
 
-def test_logger_and_exception():
-    try:
-        logging.info("starting the est_logger_and_exception")
-        result = 3/0
-        print(result)
-        logging.info("stopping the est_logger_and_exception")
-    except Exception as e:
-        logging.debug("starting the est_logger_and_exception")
-
-        raise SensorException(e,sys)
     
 if __name__=="__main__":
-    try:
-        get_collection_as_dataframe(database_name="apsSensor",collection_name="Sensor")
+        try:
+            training_pipeline_config = config_entity.TrainingPipelineConfig()
 
-    except Exception as e:
-        print(e)
+            #data ingestion
+            data_ingestion_config  = config_entity.DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+            print(data_ingestion_config.to_dict())
+            #data_ingestion = DataIngestion(data_ingestion_config=data_ingestion_config)
+            #data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        except Exception as e:
+            print(e)    
--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -1,4 +1,13 @@
-class DataIngestionConfig:...
+
+
+from dataclasses import dataclass
+
+@dataclass
+class DataIngestionArtifact:
+    feature_store_file_path:str
+    train_file_path:str 
+    test_file_path:str
+    
 class DataValidationConfig:...
 class DataTransformationConfig:...   
 class ModelTrainerConfig:...
