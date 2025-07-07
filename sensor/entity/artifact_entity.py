5c60d82775086bacfdf64e962a78e068d7c1a710
Subhankar Ghosh <subhankar333ghosh@gmail.com>
2025-07-07 21:53:42 +0530

    preparing DataTransformation

--- sensor/entity/artifact_entity.py
+++ sensor/entity/artifact_entity.py
@@ -11,8 +11,13 @@ class DataIngestionArtifact:
 class DataValidationArtifact:
     report_file_path:str
 
-
-class DataTransformationConfig:...   
+@dataclass
+class DataTransformationConfig:
+    transform_object_path:str
+    transformed_train_path:str
+    transformed_test_path:str
+    #target_encoder_path:str
+    
 class ModelTrainerConfig:...
 class ModelEvaluationConfig:...
 class ModelPusherConfig:...
